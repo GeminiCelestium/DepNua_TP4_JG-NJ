35d7b858cf737800e00b40a7584ebc8446f17353
Nathan Joseph <2145612@cegeplimoilou.ca>
2023-10-25 12:20:09 -0400

    Ajout CorrectifTP3/src/ModernRecrut.sln bis Finalisation Exercice 4 bis

---
+++ CorrectifTP3/src/ModernRecrut.Documents.API/Controllers/GestionDocumentsController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModernRecrut.Documents.API.Interfaces;
+using ModernRecrut.Documents.API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ModernRecrut.Documents.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GestionDocumentsController : ControllerBase
+    {
+        private IWebHostEnvironment _env;
+
+        private IGenererNom _genererNom;
+
+        private string DirectoryPath;
+
+        public GestionDocumentsController(IWebHostEnvironment env, IGenererNom genererNom)
+        {
+            _env = env;
+            _genererNom = genererNom;
+            DirectoryPath = Path.Combine(_env.ContentRootPath, "wwwroot\\documents");
+        }
+
+        // GET: api/<GestionDocumentsController>
+        [HttpGet("{id}")]
+        public IEnumerable<string> Get(string id)
+        {
+            var fichiersAvecChemin = Directory.GetFiles(DirectoryPath, id + "*");
+            var fichierSansChemin = new List<string>();
+            foreach (string fichier in fichiersAvecChemin)
+            {
+                fichierSansChemin.Add(Path.GetFileName(fichier));
+            }
+            return fichierSansChemin;
+        }
+
+        // POST api/<GestionDocumentsController>
+        [HttpPost]
+        public async Task<IActionResult> EnregistrementDocument(Fichier fichierRecu)
+        {
+            var codeUtilisateur = fichierRecu.Id;
+
+            byte[] bytes = Convert.FromBase64String(fichierRecu.DataFile);
+            MemoryStream stream = new MemoryStream(bytes);
+
+            IFormFile file = new FormFile(stream, 0, bytes.Length, fichierRecu.Name, fichierRecu.FileName);
+
+            try
+            {
+                Dictionary<int, string> mesDocuments = new Dictionary<int, string>();
+
+                string nom = _genererNom.GenererNomFichier(codeUtilisateur, fichierRecu.TypeDocument.ToString(), fichierRecu.FileName);
+
+                //Chemin du fichier avec son nouveau nom
+                var documentPath = Path.Combine(DirectoryPath, nom);
+
+                var fileStream = new FileStream(documentPath, FileMode.Create);
+                await file.CopyToAsync(fileStream);
+                stream.Close();
+                fileStream.Close();
+                return CreatedAtAction(nameof(EnregistrementDocument), mesDocuments);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("any/{id}")]
+        public bool AnyTypeDocumentPourUtilisateur(int id)
+        {
+            return true;
+        }
+    }
+}
---
+++ CorrectifTP3/src/ModernRecrut.Favoris.API/Controllers/GestionFavorisController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using ModernRecrut.Favoris.API.Helpers;
+using ModernRecrut.Favoris.API.Interfaces;
+using ModernRecrut.Favoris.API.Models;
+
+namespace ModernRecrut.Favoris.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GestionFavorisController : Controller
+    {        
+        private readonly IMemoryCache? _memoryCache;
+
+        private readonly IUtilitaireService _utilitaireService;
+
+        private readonly IDistributedCache _distributedCache;
+
+        private string _cacheKey;
+
+        public GestionFavorisController(IMemoryCache memoryCache, IUtilitaireService utilitaireService, IDistributedCache distributedCache)
+        {
+            _memoryCache = memoryCache;
+            _utilitaireService = utilitaireService;
+            _distributedCache = distributedCache;
+            _cacheKey = IpAdresse.GetIpAdress();
+        }
+
+        public async Task CacheRedis()
+        {
+            //Obtention des données du cache
+            string cacheDatetime = await _distributedCache.GetStringAsync("cacheDateTime");
+
+            if (cacheDatetime == null)
+            {
+                cacheDatetime = DateTime.Now.ToString();
+
+                //Définition des options de configurations du stockage des données dans le cache
+                var options = new DistributedCacheEntryOptions
+                {
+                    SlidingExpiration = TimeSpan.FromSeconds(10),
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(120),
+                };
+
+                //Ajout des données dans le cache
+                _ = _distributedCache.SetStringAsync("cacheDateTime", cacheDatetime, options);
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<OffreEmploi>>> ObtenirFavoris()
+        {
+            //Verifie si l'information existe dans le cache
+            var cache = (OffreFavoris)_memoryCache.Get(_cacheKey);
+            if (cache != null)
+            {
+                return cache.Favoris.ToList();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Ajouter(OffreEmploi offreEmploi)
+        {
+            OffreFavoris offreFavoris = (OffreFavoris)_memoryCache.Get(_cacheKey);
+            if (offreFavoris != null && !offreFavoris.Favoris.Any(o => o.Id == offreEmploi.Id))
+            {
+                offreFavoris.Favoris.Add(offreEmploi);
+                int tailleTotal = _utilitaireService.ObtenirTailleListOffreEmploi(offreFavoris.Favoris);
+                var cacheEntryOptionsTotal = new MemoryCacheEntryOptions
+                {
+                    SlidingExpiration = TimeSpan.FromHours(6),
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(24),
+                    Size = tailleTotal
+                };
+                _memoryCache.Set(_cacheKey, offreFavoris, cacheEntryOptionsTotal);
+                return Ok();
+            }
+            else
+            {
+                offreFavoris = new OffreFavoris();
+                offreFavoris.Favoris = new List<OffreEmploi>
+                {
+                    offreEmploi
+                };
+                int tailleTotal = _utilitaireService.ObtenirTailleListOffreEmploi(offreFavoris.Favoris);
+                var cacheEntryOptionsTotal = new MemoryCacheEntryOptions
+                {
+                    SlidingExpiration = TimeSpan.FromHours(6),
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(24),
+                    Size = tailleTotal
+                };
+                _memoryCache.Set(_cacheKey, offreFavoris, cacheEntryOptionsTotal);
+                return Ok();
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Supprimer(int id)
+        {
+            OffreFavoris offreFavoris = (OffreFavoris)_memoryCache.Get(_cacheKey);
+            if (offreFavoris != null && offreFavoris.Favoris.Any(o => o.Id == id))
+            {
+                var offreAEnlever = offreFavoris.Favoris.FirstOrDefault(o => o.Id == id);
+                offreFavoris.Favoris.Remove(offreAEnlever);
+                int tailleTotal = _utilitaireService.ObtenirTailleListOffreEmploi(offreFavoris.Favoris);
+                var cacheEntryOptionsTotal = new MemoryCacheEntryOptions
+                {
+                    SlidingExpiration = TimeSpan.FromHours(6),
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(24),
+                    Size = tailleTotal
+                };
+                _memoryCache.Set(_cacheKey, offreFavoris, cacheEntryOptionsTotal);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
