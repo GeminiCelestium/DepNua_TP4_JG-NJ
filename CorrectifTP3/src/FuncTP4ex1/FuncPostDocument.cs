b83f8d5958260d2ab05a3e4be295e41217e9f652
Jason1 <funhoster@hotmail.ca>
2023-10-31 18:46:17 -0400

    Function azure

---
+++ CorrectifTP3/src/FuncTP4ex1/FuncPostDocument.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Worker;
+using ModernRecrut.Documents.API.Interfaces;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.WebJobs;
+
+namespace FuncTP4ex1
+{
+
+    public class FuncPostDocument
+    {
+        private static readonly IGenererNom _genererNom;
+
+        [Function("FileUpload")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            string Connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string containerName = Environment.GetEnvironmentVariable("acremploistp4");
+            Stream myBlob = new MemoryStream();
+            var file = req.Form.Files["File"];
+            myBlob = file.OpenReadStream();
+            var blobClient = new BlobContainerClient(Connection, containerName);
+            var blob = blobClient.GetBlobClient(file.FileName);
+            await blob.UploadAsync(myBlob);
+            return new OkObjectResult("file uploaded successfylly");
+        }
+
+    }
+}
+
