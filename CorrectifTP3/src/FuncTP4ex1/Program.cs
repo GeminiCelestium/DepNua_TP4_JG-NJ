c798b3bcbe6aa95389a1f98112c832145f826959
Nathan Joseph <2145612@cegeplimoilou.ca>
2023-10-31 23:20:18 -0400

    nouveau commit

--- CorrectifTP3/src/FuncTP4ex1/Program.cs
+++ CorrectifTP3/src/FuncTP4ex1/Program.cs
@@ -1,8 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .Build();
+            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddJsonFile("config.json", optional: true, reloadOnChange: true);
+            })
+            .ConfigureServices(services =>
+            {
+                var serviceProvider = services.BuildServiceProvider();
+                var configuration = serviceProvider.GetService<IConfiguration>();
+
+                // Ajoutez la télémétrie Application Insights en utilisant IConfiguration
+                services.AddApplicationInsightsTelemetry(configuration["ApplicationInsights:InstrumentationKey"]);
+            })
+            .Build();
 
 host.Run();
+
 //nathan test8 pipeline
