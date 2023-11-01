8b9be5ef5cc6ab77be5f20b9362bb285207ce427
Nathan Joseph <2145612@cegeplimoilou.ca>
2023-10-31 23:41:54 -0400

    Commit final

--- CorrectifTP3/src/FuncTP4ex1/Program.cs
+++ CorrectifTP3/src/FuncTP4ex1/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Cela n'a pas l'air de fonctionné mais nous avons essayé de faire comme vous.
 builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
 
 var host = new HostBuilder()
