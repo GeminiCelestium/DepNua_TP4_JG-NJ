2fe17c860e00116da4737de8a8b9845a5bd4e4d6
Nathan Joseph <2145612@cegeplimoilou.ca>
2023-10-31 23:36:37 -0400

    commit

--- CorrectifTP3/src/FuncTP4ex1/Program.cs
+++ CorrectifTP3/src/FuncTP4ex1/Program.cs
@@ -1,22 +1,14 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
 
 var host = new HostBuilder()
-            .ConfigureFunctionsWorkerDefaults()
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                config.AddJsonFile("config.json", optional: true, reloadOnChange: true);
-            })
-            .ConfigureServices(services =>
-            {
-                var serviceProvider = services.BuildServiceProvider();
-                var configuration = serviceProvider.GetService<IConfiguration>();
-
-                // Ajoutez la télémétrie Application Insights en utilisant IConfiguration
-                services.AddApplicationInsightsTelemetry(configuration["ApplicationInsights:InstrumentationKey"]);
-            })
-            .Build();
+    .ConfigureFunctionsWorkerDefaults()
+    .Build();
 
 host.Run();
 
