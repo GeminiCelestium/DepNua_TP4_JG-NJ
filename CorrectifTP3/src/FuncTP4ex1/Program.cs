a7ecb2070c1e120174e097f82b82fc0ea8ca9084
Jason1 <funhoster@hotmail.ca>
2023-10-31 22:36:19 -0400

    fix

--- CorrectifTP3/src/FuncTP4ex1/Program.cs
+++ CorrectifTP3/src/FuncTP4ex1/Program.cs
@@ -1,14 +1,7 @@
-using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(services =>
-    {
-        // Register your dependencies here
-        services.AddBlobServiceClient(configuration => configuration.GetConnectionString("AzureWebJobsStorage"));
-        // Add any other necessary services
-    })
     .Build();
 
 host.Run();
